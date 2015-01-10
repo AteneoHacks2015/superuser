a3b1538328aba1103e2218bd2b19b5c8ab14299d
Kix Panganiban <panganiban@outlook.com>
2015-01-10 16:38:38 +0800

    Created base .jade files

--- studybuddy/studybuddy/views.py
+++ studybuddy/studybuddy/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def createUser(request):
-    return render(request, "page_template.jade")
+    return render(request, "user_create.jade")
